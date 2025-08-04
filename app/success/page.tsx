eacafb152a826049953116e0e778b54fe32a4a3a
MariaLipiz <marialipizfernandez@gmail.com>
2025-08-04 11:03:51 +0200

    Cambio en success

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -36,7 +36,7 @@ export default function SuccessPage() {
       <ul className="list-disc list-inside space-y-1 text-base sm:text-lg font-normal">
         <li>Humano</li>
         <li>Humanoide.</li>
-        <li>Persona con un negocio que quiera tener la seguridad de que no se va a ir a pique.</li>
+        <li>Persona con un negocio que quiera destacar y vender más.</li>
         <li>Hombre buscando hembra apta para la reproducción y para llevarse bien con tu madre.</li>
         
       </ul>
