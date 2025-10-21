e95b67ca80ffaad26e868fbebc47128bddfe44c7
MariaLipiz <marialipizfernandez@gmail.com>
2025-10-21 14:34:57 +0200

    Cambios success

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -13,11 +13,9 @@ export default function SuccessPage() {
       <h1 className="text-4xl sm:text-5xl font-bold mb-6 mb-6">Acabas de ser...</h1>
       <h1 className="text-4xl sm:text-5xl font-bold mb-6 mb-6">...</h1>
       <h1 className="text-4xl sm:text-5xl font-bold mb-6 mb-6">¡RECHAZADO!</h1>
-      <p className="text-base sm:text-lg  font-normal">
-        Mira.
-      </p>
+      
       <p className="text-base sm:text-lg font-normal">
-        Te voy a ser sincera.
+        Mira, te voy a ser sincera.
       </p>
       <p className="text-base sm:text-lg font-normal">
         Creí que la herramienta de email que tenía era una mierda y no permitia correos de confirmación, 
@@ -28,29 +26,13 @@ export default function SuccessPage() {
         Es sencillo.
       </p>
       <p className="text-base sm:text-lg font-normal">
-        Vas a tu correo, haces click en el email que acabas de recibir y ya lo tienes. 
-        Cuando confirmes tu correo, serás redirigido a una página que tendrá 
-        <strong> tu regalo de bienvenida.</strong>
+        Vas a tu bandeja de entrada, haces click en el email que acabas de recibir y ya lo tienes. 
+        Cuando confirmes tu correo, pues empezarás a recibir mis correos. No tiene más misterio.
       </p>
 
       <p className="text-base sm:text-lg  font-normal">
-        Interesante, interesante.
-      </p>
-      <p className="text-base sm:text-lg font-normal">
-        No pone gracias por suscribirte. Ni como te has suscrito te voy a jurar amor eterno.
-      </p>
-      <p className="text-base sm:text-lg font-normal">
-        Pero pone algo que si eres:
-      </p>
-      <ul className="list-disc list-inside space-y-1 text-base sm:text-lg font-normal">
-        <li>Humano</li>
-        <li>Humanoide.</li>
-        <li>Persona con un negocio que quiera destacar y vender más.</li>
-        <li>Hombre buscando hembra apta para la reproducción y para llevarse bien con tu madre.</li>
-        
-      </ul>
-      <p className="text-base sm:text-lg font-normal">
-      <strong>te va a interesar.</strong>
+        En estos correos, como te puedes imaginar, hablaremos como adultos y no encontraras prompts de ChatGPT ni cosas similares. Te lo digo por si 
+        todavía tenías alguna duda.
       </p>
 
 
@@ -77,11 +59,6 @@ export default function SuccessPage() {
       <p className="text-base sm:text-lg  font-normal">
       La vida es así.
       </p>
-
-      <p className="text-base sm:text-lg  font-normal">
-      Pasa un fantástico mediodía.
-      </p>
-
       
 
       
