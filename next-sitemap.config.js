96f39a57f8dcb32d13df8c7eec86bbdf335a3f3a
MariaLipiz <marialipizfernandez@gmail.com>
2025-10-23 16:05:56 +0200

    Cambios sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -11,7 +11,7 @@ module.exports = {
       {
         userAgent: '*',
         allow: '/',
-        disallow: ['/admin', '/api'],
+        disallow: ['/admin', '/api', '/legal'],
       },
     ],
   },
